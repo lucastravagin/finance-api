50ee995871d1165d98c51a55b688bfcf126fa4c0
Lucas Travagin Pereira <ltravagin@FINGERPRINT.LOCAL>
2020-03-19 11:41:02 -0300

    Criação do teste para inserir usuário

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const app = require('express')()
 
+const bodyParser = require('body-parser')
+
+//app.use(bodyParser.urlencoded({extend: false}))
+
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
     res.status(200).send()
 })
@@ -9,6 +15,10 @@ app.get('/users', (req, res) => {
         {name: 'Jhon Doe', mail: 'john@mail.com'}
     ]
     res.status(200).json(users)
+})
+
+app.post('/users', (req, res) => {
+    res.status(201).json(req.body)
 })
 
 module.exports = app 
--- test/user.test.js
+++ test/user.test.js
@@ -5,8 +5,17 @@ const request = require('supertest')
 test('Deve Listar Todos os usuários', () => {
     return request(app).get('/users')
         .then((res) => {
-            expect(res.status).toBe(200); //Verificar se o retorno da resposta foi 200 - ok
+            expect(res.status).toBe(200) //Verificar se o retorno da resposta foi 200 - ok
             expect(res.body).toHaveLength(1) //Verificar se o retorna da resposta veio com o tamanho do body > 1
             expect(res.body[0]).toHaveProperty('name', 'Jhon Doe') //Verificar 
+        })
+})
+
+test('Deve inserir usuário com sucesso', () => {
+    return request(app).post('/users')
+        .send({name: 'Walter Mitty', mail: 'walter@admi.com.br'})
+        .then((res) => {
+            expect(res.status).toBe(201)
+            expect(res.body.name).toBe('Walter Mitty')
         })
 })
