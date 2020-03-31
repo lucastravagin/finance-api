1d20af2c39617b5dd64cc4f6d1eb17c4d2c319d5
Lucas Travagin Pereira <ltravagin@FINGERPRINT.LOCAL>
2020-03-31 00:29:51 -0300

    Validacoes de campos para o recursos /users

--- src/services/user.js
+++ src/services/user.js
@@ -1,11 +1,18 @@
 module.exports = (app) => {
 
-    const findAll = () => {
-        return app.db('users').select()
+    const findAll = (filter = {}) => {
+        return app.db('users').where(filter).select()
     }
 
-    const save = (user) => {
-        return app.db('users').insert(user)
+    const save = async (user) => {
+        if(!user.name) return {error: 'Nome é um atriburo obrigatório'}
+        if(!user.mail) return {error: 'Email é um atriburo obrigatório'}
+        if(!user.password) return {error: 'Senha é um atriburo obrigatório'}
+
+        const userDb = await findAll({mail: user.mail})
+
+        if(userDb && userDb.length > 0) return { error: 'Já existe um usuário com esse e-mail'}
+         return app.db('users').insert(user)
     }
 
     return { findAll, save}
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -2,6 +2,8 @@ const app = require('../../src/app')
 
 const request = require('supertest')
 
+const email = `${Date.now()}@mail.com`
+
 test('Deve Listar Todos os usuários', () => {
     return request(app).get('/users')
         .then((res) => {
@@ -11,12 +13,47 @@ test('Deve Listar Todos os usuários', () => {
 })
 
 test('Deve inserir usuário com sucesso', () => {
-    const email = `${Date.now()}@mail.com`
     return request(app).post('/users')
-        .send({name: 'Walter Mitty', mail: email, password: '123456'})
+        .send({ name: 'Walter Mitty', mail: email, password: '123456' })
         .then((res) => {
             console.log(res.body)
             expect(res.status).toBe(201)
             //expect(res.body.name).toBe('Walter Mitty')
+        })
+})
+
+test('Não deve inserir usuário sem nome', () => {
+    return request(app).post('/users')
+        .send({ mail: 'lucas@email.com', password: '123456' })
+        .then((res) => {
+            expect(res.status).toBe(400)
+            expect(res.body.error).toBe('Nome é um atriburo obrigatório')
+        })
+})
+
+test('Não deve inserir usuário sem e-mail', async () => {
+    const result = await request(app).post('/users')
+        .send({ name: 'Walter Mitty', password: '123456' })
+    expect(result.status).toBe(400)
+    expect(result.body.error).toBe('Email é um atriburo obrigatório')
+})
+
+test('Não deve inserir um usuário sem senha', (done) => {
+    request(app).post('/users')
+        .send({ name: 'Walter Mitty', mail: 'email@email.com' })
+        .then(res => {
+            expect(res.status).toBe(400)
+            expect(res.body.error).toBe('Senha é um atriburo obrigatório')
+            done()
+        })
+        .catch(err => done.fail(err))
+})
+
+test('Não deve inserir usuário com e-mail já existente', () => {
+    return request(app).post('/users')
+        .send({ name: 'Walter Mitty', mail: email, password: '123456' })
+        .then((res) => {
+            expect(res.status).toBe(400)
+            expect(res.body.error).toBe('Já existe um usuário com esse e-mail')
         })
 })
